d6d9fe4ece9cf0aa86b77e03143f88a77d0d40d2
Thomas Aven <avensin@hotmail.com>
2017-02-04 20:44:52 +0100

    Use single quotation marks.

--- local.py
+++ local.py
@@ -1,26 +1,26 @@
-OWN_USERNAME = "tourniquet"
+OWN_USERNAME = 'tourniquet'
 
-WHITELISTED_COUNTRIES = ( "Sweden",
-                          "Norway",
-                          "Denmark",
-                          "Netherlands",
-                          "Finland",
-                          "Sverige" )
+WHITELISTED_COUNTRIES = ( 'Sweden',
+                          'Norway',
+                          'Denmark',
+                          'Netherlands',
+                          'Finland',
+                          'Sverige' )
 
-HIGH_PING_PLAYERS = ( "grandma",
-                     "jatuni",
-                      "Djentalist",
-                      "Nibl33t",
-                      "chaosmessenger",
-                      "Grodan",
-                      "cornflaco",
-                      "EarlyPeso",
-                      "lelkan",
-                      "Vesp",
-                      "vliegende1snor",
-                      "pwnagewolf",
-                      "Tipsi",
-                      "BK333",
-                      "w4zab1" )
+HIGH_PING_PLAYERS = ( 'grandma',
+                     'jatuni',
+                      'Djentalist',
+                      'Nibl33t',
+                      'chaosmessenger',
+                      'Grodan',
+                      'cornflaco',
+                      'EarlyPeso',
+                      'lelkan',
+                      'Vesp',
+                      'vliegende1snor',
+                      'pwnagewolf',
+                      'Tipsi',
+                      'BK333',
+                      'w4zab1' )
 
-WHITELISTED_GAMES = { "Melee": '2', }
+WHITELISTED_GAMES = { 'Melee': '2', }

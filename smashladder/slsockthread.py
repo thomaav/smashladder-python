a6a945799c809d738ca4c7cec642c8d8e9cf2349
Thomas Aven <avensin@hotmail.com>
2018-01-07 22:29:30 +0100

    Prettify SlSocketThread.

--- smashladder/slsockthread.py
+++ smashladder/slsockthread.py
@@ -13,7 +13,6 @@ class SlSocketThread(QThread):
     def __init__(self, cookie_jar=None, parent=None):
         super(SlSocketThread, self).__init__(parent)
         self.lock = threading.Lock()
-
         if cookie_jar:
             self.cookie_jar = cookie_jar
             self.username = cookie_jar['username']
@@ -31,51 +30,69 @@ class SlSocketThread(QThread):
         self.username = None
 
 
+    def auth_false(self):
+        self.qt_print.emit('Authentication false, exiting')
+        self.ws.close()
+
+
+    def process_private_chat_message(self, raw_message):
+        processed_message = sl.process_private_chat_message(raw_message)
+        self.qt_print.emit(processed_message['info'])
+
+
+    def process_match_message(self, raw_message):
+        processed_message = sl.process_match_message(raw_message)
+        if 'Entered match' in processed_message['info']:
+            self.entered_match.emit(processed_message['match_id'])
+            return
+
+        if not processed_message['typing']:
+            self.qt_print.emit(processed_message['info'])
+
+
+    def process_open_challenges(self, raw_message):
+        try:
+            processed_message = sl.process_open_challenges(self.cookie_jar, raw_message)
+        except slexceptions.RequestTimeoutException as e:
+            self.qt_print.emit(str(e))
+            return
+
+        if processed_message['match_id']:
+            self.qt_print.emit(processed_message['info'])
+
+        if 'Accepted challenge' in processed_message['info']:
+            self.entered_match.emit(processed_message['match_id'])
+
+
+    def process_new_search(self, raw_message):
+        if builtins.in_match:
+            return
+
+        try:
+            player = sl.process_new_search(self.cookie_jar, raw_message, self.username)
+        except slexceptions.RequestTimeoutException as e:
+            self.qt_print.emit(str(e))
+            return
+
+        if player:
+            self.qt_print.emit('Challenging ' + player['username'] + ' from ' + player['country'])
+
     def on_message(self, ws, raw_message):
         with self.lock:
             if '\"authentication\":false' in raw_message:
-                self.qt_print.emit('Authentication false, exiting')
-                self.ws.close()
+                self.auth_false()
 
             elif 'private_chat' in raw_message:
-                processed_message = sl.process_private_chat_message(raw_message)
-                self.qt_print.emit(processed_message['info'])
+                self.process_private_chat_message(raw_message)
 
             elif 'current_matches' in raw_message:
-                processed_message = sl.process_match_message(raw_message)
-
-                if 'Entered match' in processed_message['info']:
-                    self.entered_match.emit(processed_message['match_id'])
-                    return
-
-                if not processed_message['typing']:
-                    self.qt_print.emit(processed_message['info'])
+                self.process_match_message(raw_message)
 
             elif 'open_challenges' in raw_message:
-                try:
-                    processed_message = sl.process_open_challenges(self.cookie_jar, raw_message)
-                except slexceptions.RequestTimeoutException as e:
-                    self.qt_print.emit(str(e))
-                    return
-
-                if processed_message['match_id']:
-                    self.qt_print.emit(processed_message['info'])
-
-                if 'Accepted challenge' in processed_message['info']:
-                    self.entered_match.emit(processed_message['match_id'])
+                self.process_open_challenges(raw_message)
 
             elif 'searches' in raw_message:
-                if builtins.in_match:
-                    return
-
-                try:
-                    player = sl.process_new_search(self.cookie_jar, raw_message, self.username)
-                except slexceptions.RequestTimeoutException as e:
-                    self.qt_print.emit(str(e))
-                    return
-
-                if player:
-                    self.qt_print.emit('Challenging ' + player['username'] + ' from ' + player['country'])
+                self.process_new_search(raw_message)
 
 
     def on_error(self, ws, error):
